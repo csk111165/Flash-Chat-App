fbaf0e28dad2b03460e0f57c2ab93a82d9a963db
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-05 00:19:42 +0530

    Refactoring out the button to reuse it for login and logout both

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -107,25 +107,7 @@ static const colorizeTextStyle = TextStyle(
             const SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: Material(
@@ -146,6 +128,37 @@ static const colorizeTextStyle = TextStyle(
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedButton extends StatelessWidget {
+  
+  final Color colour;
+  final String title;
+  final VoidCallback onPressed;
+
+  // define our custom constructor
+
+  RoundedButton({required this.colour, required this.title, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: colour,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child:  Text(
+            title,
+          ),
         ),
       ),
     );
