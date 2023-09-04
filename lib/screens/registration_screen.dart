1d2e8e6e871955032124634d091ea939b693dc2c
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-05 05:12:06 +0530

    Adding authentication

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:chat_app/components/rounded_button.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import '../constants.dart';
 
 class RegistrationScreen extends StatefulWidget {
@@ -14,6 +14,8 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   late String email;
   late String password;
+  final _auth = FirebaseAuth.instance;
+
 
   @override
   Widget build(BuildContext context) {
@@ -66,9 +68,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             RoundedButton(
               title: 'Register',
               colour: Colors.blueAccent,
-              onPressed: () {
+              onPressed: () async {
                 print(email);
                 print(password);
+               final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
               },
             )
           ],
