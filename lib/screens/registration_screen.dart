4a4b344ca402944c4674b98578c6a9fe84c6b56a
Chandra Shekhar <cshekhar@nvidia.com>
2023-09-05 08:35:46 +0530

    Introducing spinner while user make a register request

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:chat_app/components/rounded_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../constants.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart'; // for spinner
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -16,75 +17,84 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   late String email;
   late String password;
   final _auth = FirebaseAuth.instance;
-
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                //Do something with the user input(email).
-                email = value;
-              },
-              // copyWith copies the property but allows the other attrib to get modified too, like here hinttext
-              // this would allow us to use the same componemtn with slightly varying property.
-              decoration:
-                  kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input(password).
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your password'),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              title: 'Register',
-              colour: Colors.blueAccent,
-              onPressed: () async {
-                print(email);
-                print(password);
-                try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                    if( newUser != null ) {
-                        Navigator.pushNamed(context, ChatScreen.id);
-                    }
-                }
-                catch(e) {
-                  print(e);
-                }
-               
-              },
-            )
-          ],
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  //Do something with the user input(email).
+                  email = value;
+                },
+                // copyWith copies the property but allows the other attrib to get modified too, like here hinttext
+                // this would allow us to use the same componemtn with slightly varying property.
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  //Do something with the user input(password).
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your password'),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                title: 'Register',
+                colour: Colors.blueAccent,
+                onPressed: () async {
+                   // as soon as register button is pressed we need to start showing the spinner..
+                   setState(() {
+                     showSpinner = true; // 👉🏼 but if we keep it true forever it would go and spin 
+                   });
+                  try {
+                      final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                      if( newUser != null ) {
+                          Navigator.pushNamed(context, ChatScreen.id);
+                      }
+
+                      setState(() {
+                        showSpinner = false; // 👉🏼 make it false here since user is created now
+                      });
+                  }
+                  catch(e) {
+                    print(e);
+                  }
+                 
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
